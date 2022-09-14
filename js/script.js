62bbf30e66aebeb3e1de97d25e95b04c5a310a68
AlessandroRamoni <alessandroramoni@yahoo.it>
2022-09-14 18:33:47 +0200

    add aggiungiAttivita function

--- js/script.js
+++ js/script.js
@@ -13,6 +13,9 @@ MILESTONE 2
 Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo viene rimosso dalla lista.
 MILESTONE 3
 Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
+Bonus:
+1- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il todo alla lista
+2- cliccando sul testo dell'item, invertire il valore della proprietà done del todo corrispondente (se done era uguale a false, impostare true e viceversa)
 */
 
 const app = new Vue({
@@ -41,6 +44,8 @@ const app = new Vue({
       },
     ],
     attivitaArchiviate: [],
+
+    ////// deve essere collegata con il v-model?
     nuovaAttivita: "",
   },
   methods: {
@@ -54,7 +59,11 @@ const app = new Vue({
       const attivitaDaAggiungere = this.nuovaAttivita;
       this.nuovaAttivita = "";
       if (attivitaDaAggiungere.length > 0) {
-        this.planningGiornata.push(attivitaDaAggiungere);
+        let nuovoOggetto = {
+          text: attivitaDaAggiungere,
+          done: false,
+        };
+        this.planningGiornata.push(nuovoOggetto);
         console.log(this.planningGiornata);
         console.log(attivitaDaAggiungere);
       } else {
